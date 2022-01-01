a633a7c24b2e3680dd28cf4f647b180a0410da78
Suat <hekimoglu9632@gmail.com>
2022-01-01 15:31:47 +0300

    app development completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+// Libraries
+import { Router, Route, Switch } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+// Components
+import Navbar from "./components/navbar.component";
+import ExercisesList from "./components/exercises-list.component";
+import EditExercise from "./components/edit-exercise.component";
+import CreateExercise from "./components/create-exercise.component";
+import CreateUser from "./components/create-user.component";
+
+// Helpers
+import { createBrowserHistory } from "history";
+export const history = createBrowserHistory();
+
+history.listen((location, action) => {
+  // location is an object like window.location
+  console.log(action, location.pathname, location.state)
+});
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router history={history}>
+      <div className="container">
+        <Navbar />
+        <br />
+        <Switch>
+          <Route path="/" exact component={ExercisesList} />
+          <Route path="/edit/:id" exact component={EditExercise} />
+          <Route path="/create" exact component={CreateExercise} />
+          <Route path="/user" exact component={CreateUser} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
